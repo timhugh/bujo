fc1c1fdad303e3dc56db177044a65411b4811774
Tim Heuett <tim.heuett@gmail.com>
2026-02-06 16:09:16 -0800

    add basic template support

--- include/bujo/templates.hpp
+++ include/bujo/templates.hpp
@@ -1,13 +1,51 @@
 #pragma once
 
+#include "bujo/date.hpp"
+#include <date/date.h>
 #include <inja/environment.hpp>
+#include <inja/function_storage.hpp>
 #include <inja/inja.hpp>
+#include <sstream>
 #include <string>
 
 namespace bujo::templates {
 
-inline const std::string render(const std::string &tmpl) {
-  return inja::render(tmpl, {});
+namespace {
+
+inline inja::json to_json(const date::Span &span) {
+  std::vector<inja::json> days;
+  for (auto day : span.days()) {
+    days.push_back({
+        {"year", day.format("%Y")},    //
+        {"month", day.format("%m")},   //
+        {"day", day.format("%d")},     //
+        {"weekday", day.format("%A")}, //
+        {"iso", day.format("%F")}      //
+    });
+  }
+  return {{"days", days}};
+}
+
+inline std::string date_format(const std::string &iso_date,
+                               const std::string &format) {
+  std::istringstream in(iso_date);
+  ::date::year_month_day ymd;
+  in >> ::date::parse("%F", ymd);
+  return date::format(format, ymd);
+}
+
+} // namespace
+
+inline const std::string render(const std::string &tmpl,
+                                const date::Span &span) {
+  inja::Environment env;
+
+  env.add_callback("date_format", [](const inja::Arguments &args) {
+    return date_format(args.at(0)->get<std::string>(),
+                       args.at(1)->get<std::string>());
+  });
+
+  return env.render(tmpl, {{"span", to_json(span)}});
 }
 
 } // namespace bujo::templates
--- tests/test_templates.cpp
+++ tests/test_templates.cpp
@@ -1,13 +1,57 @@
 #include <gtest/gtest.h>
 
+#include "bujo/date.hpp"
 #include "bujo/templates.hpp"
 
 using namespace bujo;
+using namespace bujo::date;
+using namespace bujo::templates;
 
-TEST(TemplatesTest, RenderSpanDates) {
-  const std::string templ = "";
+class TemplatesTest_Render : public ::testing::Test {
+protected:
+  // week 6 of 2026
+  local_days start{year{2026} / month{2} / day{2}};
+  local_days end{year{2026} / month{2} / day{8}};
 
-  auto rendered = templates::render(templ);
+  Span span{start, end};
+};
 
-  EXPECT_EQ("", rendered);
+TEST_F(TemplatesTest_Render, RenderWithSpanData) {
+  std::string tmpl = R"(
+  {%- for day in span.days -%}
+  {{ day.year }}-{{ day.month }}-{{ day.day }} {{ day.weekday }}
+	{%- endfor -%}
+  )";
+
+  std::string expected = R"(
+2026-02-02 Monday
+2026-02-03 Tuesday
+2026-02-04 Wednesday
+2026-02-05 Thursday
+2026-02-06 Friday
+2026-02-07 Saturday
+2026-02-08 Sunday
+)";
+
+  EXPECT_EQ(render(tmpl, span), expected);
+}
+
+TEST_F(TemplatesTest_Render, RenderDateFormatCallback) {
+  std::string tmpl = R"(
+  {%- for day in span.days -%}
+	  {{ date_format(day.iso, "%Y-%m-%d %A") }}
+	{%- endfor -%}
+	)";
+
+  std::string expected = R"(
+2026-02-02 Monday
+2026-02-03 Tuesday
+2026-02-04 Wednesday
+2026-02-05 Thursday
+2026-02-06 Friday
+2026-02-07 Saturday
+2026-02-08 Sunday
+)";
+
+  EXPECT_EQ(render(tmpl, span), expected);
 }
